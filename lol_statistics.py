33f70b884e4a2d69480de1277c22eebdc46381fb
Gomchichit <sammiffy77@hanmail.net>
2018-12-20 17:02:52 +0900

    주석 추가

--- lol_statistics.py
+++ lol_statistics.py
@@ -11,27 +11,33 @@ from math import floor
 
 app = Flask(__name__)
 
-slack_token = "xoxb-503818135714-507351131987-GyVOyX3uF6Pzv1N4yT5X9dKn"
+slack_token = "xoxb-503818135714-507351131987-KC8Tcl563Z9NuL505vFJK4bF"
 slack_client_id = "503818135714.507348823507"
 slack_client_secret = "8194f60277b7af2f46584293915e356b"
 slack_verification = "hgtt6xtPYcb4Oq5TzDPhYFOr"
 sc = SlackClient(slack_token)
 slack = Slacker(slack_token)
 
+'''
+챔피언을 추천해달라는 명령어가 입력되어 추천 인텐트가 실행되었을 때 호출되는 함수로
+챔피언의 승률과 픽률을 기반으로 추천 지수를 계산하여 추천 지수가 높은 10개 챔피언의 목록을
+반환하는 함수
+'''
 def crawl_opgg_recommend(intent):
+    # 엉뚱한 명령어에 대한 호출이 발생되면 아무것도 반환하지 않는다.
     if intent == 'Default Fallback Intent':
         return ''
 
     keywords = []
     statistics = []
 
+    # 승/픽률 페이지에서 데이터를 받아온다.
     sourcecode1 = urllib.request.urlopen(
         'http://www.op.gg/champion/ajax/statistics/trendChampionList/type=winratio&').read()
     soup1 = BeautifulSoup(sourcecode1, "html.parser")
-    sourcecode2 = urllib.request.urlopen(
-        'http://www.op.gg/champion/ajax/statistics/trendChampionList/type=banratio&').read()
-    soup2 = BeautifulSoup(sourcecode2, "html.parser")
 
+    # 승/픽률을 기반으로 한 챔피언 목록과 승/픽률을 각 리스트에 받아온다.
+    # 이때, 페이지 내에 같은 유형의 데이터가 반복되므로 정해진 챔피언의 개수만큼 리스트들을 슬라이싱 한다.
     winpick_champ = [i.get_text() for i in soup1.find_all("div", class_="champion-index-table__name")]
     winpick_champ = winpick_champ[:142]
     winratio = [i.get_text() for i in soup1.find_all("td",
@@ -41,6 +47,12 @@ def crawl_opgg_recommend(intent):
                  soup1.find_all("td", class_="champion-index-table__cell champion-index-table__cell--value")]
     pickratio = pickratio[:142]
 
+    # 챔피언의 이름과 승률, 픽률을 이용한 추천 지수를 계산 한다. 이때, 자료는 스트링 형태의 데이터로 저장되어 있으므로
+    # 뒤에 붙어있는 %를 없애준 후 float 형태로 캐스팅 해준다.
+    # 계산된 추천 지수를 이용하여 (챔피언 이름, 추천 지수) 형태의 튜플 자료형으로 저장한다.
+    # 결과를 추천 지수별로 정렬하기 위해 튜플의 인자 중 2번째 인자를 이용한다.
+    # 정렬된 결과 중 상위 10개의 결과들만 키워드 리스트에 형식을 맞추어 저장해준다.
+    # 가장 추천 지수가 높은 챔피언의 초상화를 찾아 이미지를 보여준다.
     for i in range(len(winpick_champ)):
         if not winpick_champ[i] in statistics:
             statistics.append((winpick_champ[i], floor(float(winratio[i].strip('%')) * float(pickratio[i].strip('%'))) / 100))
@@ -51,7 +63,14 @@ def crawl_opgg_recommend(intent):
     slack.files.upload(statistics[0][0].replace(" ", "") + '.png', channels="day4")
 
     return u'\n'.join(keywords)
+
+'''
+챔피언을 추천해달라는 명령어 이외의 명령어에 대한 작업을 수행하는 함수로
+승률, 픽률, 밴률을 기반으로 각 승, 픽, 밴 인텐트가 호출 되고 인텐트의 종류에 따라
+반환하는 상위 10개 챔피언의 정렬 기준도 승률, 픽률, 밴률에 따라 달라진다.
+'''
 def crawl_opgg_top10(intent):
+    # 추천 지수 함수와 같은 방식으로 작성
     if intent == 'Default Fallback Intent':
         return ''
 
@@ -62,6 +81,8 @@ def crawl_opgg_top10(intent):
     sourcecode1 = urllib.request.urlopen(
         'http://www.op.gg/champion/ajax/statistics/trendChampionList/type=winratio&').read()
     soup1 = BeautifulSoup(sourcecode1, "html.parser")
+
+    # 밴률 페이지가 따로 존재하므로 밴 페이지를 따로 받아준다.
     sourcecode2 = urllib.request.urlopen(
         'http://www.op.gg/champion/ajax/statistics/trendChampionList/type=banratio&').read()
     soup2 = BeautifulSoup(sourcecode2, "html.parser")
@@ -89,6 +110,7 @@ def crawl_opgg_top10(intent):
         if not ban_champ[i] in statistics:
             statistics_ban.append((ban_champ[i], float(banratio[i].strip('%'))))
 
+    # 인텐트의 내용에 따라 승, 픽, 밴인지 나누어서 출력되는 결과를 다르게 한다.
     if intent == '승':
         statistics = sorted(statistics, key=itemgetter(1), reverse=True)
         keywords = ["챔피언 승률 Top 10"]
@@ -98,7 +120,7 @@ def crawl_opgg_top10(intent):
     if intent == '픽':
         statistics = sorted(statistics, key=itemgetter(2), reverse=True)
         keywords = ["챔피언 픽률 Top 10"]
-        slack.files.upload(statistics[0][0].replace(" ", "") + '.png', channels="day4")
+        slack.files.upload(statistics[0][0].replace(" ", "") + '.png', channels="day3")
         for i in range(1, 11):
             keywords.append(str(i) + "위: " + statistics[i-1][0] + "\t\t픽률: " + str(statistics[i-1][2]) + "%")
     if intent == '밴':
@@ -148,6 +170,8 @@ def _event_handler(event_type, slack_event):
         text = text[13:]
         answer = get_answer(text, 'session')
 
+        # 명령어가 요구하는 사항에 따라 인텐트를 다르게 하여 추천 지수 함수로 갈 것인지
+        # 승, 픽, 밴률 탑텐 함수로 갈 것인지 결정한다.
         if answer['intent'] == '추천':
             keywords = crawl_opgg_recommend(answer['intent'])
         else:
